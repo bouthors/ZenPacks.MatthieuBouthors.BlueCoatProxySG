8b878f7f639550d219d0131bf7c21f0b2f1b0603
Matthieu Bouthors <matthieu@bouthors.fr>
2015-12-17 04:51:47 +0100

    update modeler

---
+++ ZenPacks/MatthieuBouthors/BlueCoatProxySG/modeler/plugins/MatthieuBouthors/snmp/BC_SG_Usage.py
@@ -0,0 +1,39 @@
+from Products.DataCollector.plugins.CollectorPlugin import (
+    SnmpPlugin, GetTableMap,
+    )
+
+
+class ProxySG(SnmpPlugin):
+    relname = 'blueCoatSGUsage'
+    modname = 'ZenPacks.MatthieuBouthors.BlueCoatProxySG.BlueCoatSGUsage'
+
+
+    snmpGetTableMaps = (
+        GetTableMap(
+            'tempUsageTable', '.1.3.6.1.4.1.3417.2.4.1.1.1', {
+                '.3': 'deviceUsageName',
+                }
+            ),
+        )
+
+    def process(self, device, results, log):
+        temp_Usage = results[1].get('tempUsageTable', {})
+	log.info(temp_Usage)
+
+	rm = self.relMap()
+        for snmpindex, row in temp_Usage.items():
+            name = row.get('deviceUsageName')
+            if not name:
+                log.warn('Skipping usage with no name')
+                continue
+
+            rm.append(self.objectMap({
+                'id': self.prepId(name),
+                'deviceUsageName': name,
+                'snmpindex': snmpindex.strip('.'),
+                }))
+	log.info(rm)
+
+        log(rm)
+        return rm
+
